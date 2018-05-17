ae2f004c7493b3225c8c96d4426d7f56c882fef6
Kanwar <kanwsingh@deloitte.com>
2018-05-17 21:53:20 +0530

    Fixed Actual and Expected results

--- test/delivery/deliveryCost.js
+++ test/delivery/deliveryCost.js
@@ -1,69 +1,68 @@
- let expect = require('chai').expect
-import {DeliveryCost} from '../../server/delivery/deliveryCost'
+let expect = require('chai').expect
+import { DeliveryCost } from '../../server/delivery/deliveryCost'
 
 describe('Case 1 : findCostOfDelivery in a specific route', () => {
-    it('should return cost of delivery for route E-A-C-F', function () {
+    it('should return 8 as cost of delivery for route E-A-C-F', function () {
 
-     
+
         let route = ('E-A-C-F')
         let expectedCost = 8;
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
-        let cD  = new DeliveryCost(inputGraph);
+        let cD = new DeliveryCost(inputGraph);
         let actualCost = cD.findCostOfDelivery(route);
-        expect(actualCost).to.be.equal(expectedCost);
+        expect(expectedCost).to.be.equal(actualCost);
     });
 
     it('should return cost of delivery for route A-D', function () {
 
-        
+
         let route = ('A-D')
         let expectedCost = 10;
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
-        let cD  = new DeliveryCost(inputGraph);
+        let cD = new DeliveryCost(inputGraph);
         let actualCost = cD.findCostOfDelivery(route);
-        expect(actualCost).to.be.equal(expectedCost);
+        expect(expectedCost).to.be.equal(actualCost);
     });
 
-    it('should return cost of delivery for  route A-B-E', ()=> {
+    it('should return 4 as cost of delivery for  route A-B-E', () => {
 
         // 1. ARRANGE
         let route = ('A-B-E')
         let expectedCost = 4;
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
-        let cD  = new DeliveryCost(inputGraph);
+        let cD = new DeliveryCost(inputGraph);
         let actualCost = cD.findCostOfDelivery(route);
-        expect(actualCost).to.be.equal(expectedCost);
+        expect(expectedCost).to.be.equal(actualCost);
     });
-    it('should return no such route for A-D-F',  () =>{
+    it('should return no such route for A-D-F', () => {
 
-        
+
         let route = ('A-D-F')
         let expectedCost = 'No Such Route';
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
-        let cD  = new DeliveryCost(inputGraph);
+        let cD = new DeliveryCost(inputGraph);
         let actualCost = cD.findCostOfDelivery(route);
 
-        expect(actualCost).to.be.equal(expectedCost);
+        expect(expectedCost).to.be.equal(actualCost);
     });
-    it('should return no such route for A',  ()=> {
+    it('should return no such route for A', () => {
 
-        
         let route = ('A')
         let expectedCost = 'No Such Route';
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
-        let cD  = new DeliveryCost(inputGraph);
+        let cD = new DeliveryCost(inputGraph);
         let actualCost = cD.findCostOfDelivery(route);
-        expect(actualCost).to.be.equal(expectedCost);
+        expect(expectedCost).to.be.equal(actualCost);
     });
-    it('should return no such route for empty route', ()=> {
+    it('should return no such route for empty route', () => {
 
-        
+
         let route = ('')
         let expectedCost = 'No Such Route';
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
-        let cD  = new DeliveryCost(inputGraph);
+        let cD = new DeliveryCost(inputGraph);
         let actualCost = cD.findCostOfDelivery(route);
-        expect(actualCost).to.be.equal(expectedCost);
+        expect(expectedCost).to.be.equal(actualCost);
     });
 });
 
--- test/delivery/possibleRoutes.js
+++ test/delivery/possibleRoutes.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect
 import { PossibleRoutes } from '../../server/delivery/possibleRoutes'
 
 describe('Case 2 : findNumberPossibleRoutes from one Town to another Town', () => {
-    it('should return number of possible routes from E to D', function () {
+    it('should return 4 as possible routes from E to D with max stops 4',  () => {
 
 
         const fromTown = 'E'
@@ -16,10 +16,10 @@ describe('Case 2 : findNumberPossibleRoutes from one Town to another Town', () =
         const actualRoutes = pR.findNumberPossibleRoutes(fromTown, endTown, maxStops, sameRouteTwice)
 
 
-        expect(actualRoutes).to.be.equal(expectedRoutes);
+        expect(expectedRoutes).to.be.equal(actualRoutes);
     });
 
-    it('should return number of possible routes from E to E', function () {
+    it('should return 5 as possible routes from E to E with infinite stops',  () => {
 
 
         const fromTown = 'E'
@@ -32,24 +32,68 @@ describe('Case 2 : findNumberPossibleRoutes from one Town to another Town', () =
 
         const actualRoutes = pR.findNumberPossibleRoutes(fromTown, endTown, maxStops, sameRouteTwice)
 
-        expect(actualRoutes).to.be.equal(expectedRoutes);
+        expect(expectedRoutes).to.be.equal(actualRoutes);
     });
-    it('should return number of possible routes from E to nothing', function () {
+    it('should return 0 as possible routes from A to nothing with infinite stops',  () => {
 
 
         const fromTown = 'E'
-        const endTown = 'E'
+        const endTown = ''
         const maxStops = Number.MAX_SAFE_INTEGER
-        const expectedRoutes = 5;
+        const expectedRoutes = 0;
         const sameRouteTwice = false
         let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
         let pR = new PossibleRoutes(inputGraph);
 
-        const actualRoutes = pR.findNumberPossibleRoutes(fromTown, '', maxStops, sameRouteTwice)
+        const actualRoutes = pR.findNumberPossibleRoutes(fromTown, endTown, maxStops, sameRouteTwice)
+        
+        expect(expectedRoutes).to.be.equal(actualRoutes);
+    });
+    
+    it('should return 1 as possible routes from A to F with max stops 4',  () => {
 
-        expect(actualRoutes).to.be.equal(expectedRoutes);
+
+        const fromTown = 'A'
+        const endTown = 'F'
+        const maxStops = 4
+        const expectedRoutes = 1;
+        const sameRouteTwice = false
+        let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
+        let pR = new PossibleRoutes(inputGraph);
+
+        const actualRoutes = pR.findNumberPossibleRoutes(fromTown, endTown, maxStops, sameRouteTwice)
+        expect(expectedRoutes).to.be.equal(actualRoutes);
     });
 
+    it('should return 0 as possible routes from F to X with max stops 4',  () => {
+
+
+        const fromTown = 'F'
+        const endTown = 'X'
+        const maxStops = 4
+        const expectedRoutes = 0;
+        const sameRouteTwice = false
+        let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
+        let pR = new PossibleRoutes(inputGraph);
+
+        const actualRoutes = pR.findNumberPossibleRoutes(fromTown, endTown, maxStops, sameRouteTwice)
+        expect(expectedRoutes).to.be.equal(actualRoutes);
+    });
+
+    it('should return 0 as possible routes from X to F with max stops 4',  () => {
+
+
+        const fromTown = 'F'
+        const endTown = 'X'
+        const maxStops = 4
+        const expectedRoutes = 0;
+        const sameRouteTwice = false
+        let inputGraph = 'AB1, AC4, AD10, BE3, CD4, CF2, DE1, EB3, EA2, FD1'
+        let pR = new PossibleRoutes(inputGraph);
+
+        const actualRoutes = pR.findNumberPossibleRoutes(fromTown, endTown, maxStops, sameRouteTwice)
+        expect(expectedRoutes).to.be.equal(actualRoutes);
+    });
 
 });
 
