cc13a011afaf974eab0287c4383f2daff8f9e418
Kanwarjeet singh <kanwarjeet.cs10@iitp.ac.in>
2018-07-17 14:20:06 +0530

    Update server.js

--- server/api/server.js
+++ server/api/server.js
@@ -1,5 +1,5 @@
 import app from './app'
-let port = process.env.port || 8080;
+let port = process.env.port || 80;
 
 let server = app.listen(port,()=>{
     console.log(`Eko Delivery App is running on ${port}`);
